c3a6a6017f0229ac0f30b239e34c23d94bb8edf8
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2022-02-01 19:51:46 -0300

    90% Cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,52 @@
 const cipher = {
-  // ...
+  encode(offset, mensagem) {
+    // se offset for numero, nao da erro
+    // se offset nao for numero, da erro
+    if (typeof offset !== "number") {
+      throw new TypeError("offset não é um número");
+    }
+
+    if (typeof mensagem !== "string") {
+      throw new TypeError("mensagem não é uma string");
+    }
+
+    let quantidadeLetras = mensagem.length;
+    let resultadoCifra = "";
+
+    for (let i = 0; i < quantidadeLetras; i++) {
+      let codigoASC = mensagem.charCodeAt(i);
+      let codigoA = 65;
+      let cifrar = (codigoASC - codigoA + offset) % 26 + codigoA;
+      resultadoCifra = resultadoCifra.concat(String.fromCharCode(cifrar))
+    }
+    return resultadoCifra
+  },
+
+  decode(offset, mensagem) {
+
+    if (typeof offset !== "number") {
+      throw new TypeError("offset não é um número");
+    }
+
+    if (typeof mensagem !== "string") {
+      throw new TypeError("mensagem não é uma string");
+    }
+
+    let quantidadeLetras = mensagem.length;
+    let resultadoCifra = "";
+
+    for (let i = 0; i < quantidadeLetras; i++) {
+      let codigoASC = mensagem.charCodeAt(i);
+      let codigoZ = 90;
+      let cifrar = (codigoASC - codigoZ - offset) % 26 + codigoZ;
+      resultadoCifra = resultadoCifra.concat(String.fromCharCode(cifrar))
+    }
+    return resultadoCifra
+
+
+  }
 };
 
 export default cipher;
+
+
